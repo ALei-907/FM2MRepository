384348554c68823874fd773fdbce177c8773f253
leilimin <461083868@qq.com>
2022-04-19 23:37:37 +0800

    style(docker): 文件夹修改

---
+++ JavaUtils/src/main/java/test.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+/**
+ * @Author LeiLiMin
+ * @Description:
+ * @date: 2022/4/18
+ */
+public class test {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        Integer integer = integers.get(0);
+        integer=2;
+        System.out.println(integers);
+    }
+}
