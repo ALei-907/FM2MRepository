7c1e300a7b0cd0fbef6901e6985bd289ba60b1b9
leilimin <461083868@qq.com>
2022-04-22 22:18:55 +0800

    feat(test): bigDecimal

--- JavaUtils/src/main/java/test.java
+++ JavaUtils/src/main/java/test.java
@@ -1,5 +1,7 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author LeiLiMin
@@ -13,5 +15,24 @@ public class test {
         Integer integer = integers.get(0);
         integer=2;
         System.out.println(integers);
+
+        BigDecimal a = new BigDecimal(1.1);
+        BigDecimal b = new BigDecimal(2);
+        BigDecimal c = new BigDecimal(3);
+        BigDecimal add = a.multiply(b).add(c);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(3,3);
+        map.put(1,1);
+        map.put(2,2);
+        map.put(1,1);
+        map.put(4,4);
+        map.put(5,5);
+        for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
+            System.out.println(integerIntegerEntry.getKey());
+        }
+
+
+
     }
 }
