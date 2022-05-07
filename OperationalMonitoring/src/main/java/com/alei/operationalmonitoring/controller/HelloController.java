bf2aed1d93f5f777c2b38ed3717c1fa006f81fb6
leilimin <461083868@qq.com>
2022-05-07 18:55:53 +0800

    feat(prometheus): 自定义服务Init

--- OperationalMonitoring/src/main/java/com/alei/operationalmonitoring/controller/HelloController.java
+++ OperationalMonitoring/src/main/java/com/alei/operationalmonitoring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.alei.operationalmonitoring.controller;
 
+import io.prometheus.client.Counter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1")
 public class HelloController {
+    static final Counter requests = Counter.build()
+            .name("requests_total").help("Total requests.").register();
+
 
     @GetMapping("/Hello")
     public String hello(@RequestParam String clientName) {
+        requests.inc();
         return "Hello " + clientName;
     }
 }
