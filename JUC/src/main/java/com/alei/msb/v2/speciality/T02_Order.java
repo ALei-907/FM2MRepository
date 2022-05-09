b93557e1d52efa74d188483cca0e44fe83eb2e6d
leilimin <461083868@qq.com>
2022-05-09 23:36:46 +0800

    feat(JUC): speciality
    1.DisOrder

--- JUC/src/main/java/com/alei/msb/v2/speciality/T02_Order.java
+++ JUC/src/main/java/com/alei/msb/v2/speciality/T02_Order.java
@@ -17,9 +17,9 @@ public class T02_Order {
     /**
      * 乱序验证
      * 为什么会出现乱序?
-     *  *: 提高效率,类似与IO与CPU
+     * *: 提高效率,类似与IO与CPU
      * 乱序出现的条件
-     *  *: 前后语句不存在依赖关系"不影响单线程的最终一致性"
+     * *: 前后语句不存在依赖关系"不影响单线程的最终一致性"
      */
     private static int x = 0, y = 0;
     private static int a = 0, b = 0;
@@ -50,5 +50,23 @@ public class T02_Order {
                 System.out.println("x=0,y=0");
             }
         }
+    }
+
+
+    private static boolean ready = false;
+    private static int number;
+
+    public static void disOrder2() throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(number);
+        });
+        t1.start();
+        // number 和 ready 或许会更改顺序
+        number = 42;
+        ready = true;
+        t1.join();
     }
 }
