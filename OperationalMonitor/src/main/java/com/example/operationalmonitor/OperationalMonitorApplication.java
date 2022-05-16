acba81ba1612a0c8203f9d437b2c1983e4e60c7b
leilimin <461083868@qq.com>
2022-05-16 11:31:22 +0800

    feat(prometheus): refresh

---
+++ OperationalMonitor/src/main/java/com/example/operationalmonitor/OperationalMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.operationalmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationalMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationalMonitorApplication.class, args);
+    }
+
+}
