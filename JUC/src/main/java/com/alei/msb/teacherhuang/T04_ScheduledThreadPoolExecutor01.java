c24134b0084497b8b3ed3c9684b2ce17f502d0ff
leilimin <461083868@qq.com>
2022-05-23 23:05:26 +0800

    Update(JUC): ScheduledThreadPoolExecutor Doc

--- JUC/src/main/java/com/alei/msb/teacherhuang/T04_ScheduledThreadPoolExecutor01.java
+++ JUC/src/main/java/com/alei/msb/teacherhuang/T04_ScheduledThreadPoolExecutor01.java
@@ -12,11 +12,19 @@ import java.util.concurrent.TimeUnit;
  */
 public class T04_ScheduledThreadPoolExecutor01 {
     public static void main(String[] args) {
+        // 1.构造器相关
+        /**
+         * 1.新增了默认Worker的销毁时间
+         * 2.内部设置的Queue-{@link DelayedWorkQueue},是一个无界队列
+         *   所以内部设置的最大线程数为Integer.MAX，其实是不生效的
+         */
         ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
-        scheduledExecutorService.schedule(() -> {
-            System.out.println("scheduled");
-        }, 10, TimeUnit.SECONDS);
-        scheduledExecutorService.execute(()->{});
+
+
+        scheduledExecutorService.schedule(() -> System.out.println("scheduled"), 10, TimeUnit.SECONDS);
+        scheduledExecutorService.execute(() -> System.out.println("scheduled"));
+
+
         // 任务执行时开始计时
         scheduledExecutorService.scheduleAtFixedRate(() -> {
             System.out.println(new Date());
