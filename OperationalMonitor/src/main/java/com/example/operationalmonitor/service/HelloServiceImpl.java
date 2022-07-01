12510a0a450423ffb10e8bb8ddb5e914ce96ce63
leilimin <461083868@qq.com>
2022-07-01 22:52:17 +0800

    update(prometheus): Custom Counter

--- OperationalMonitor/src/main/java/com/example/operationalmonitor/service/HelloServiceImpl.java
+++ OperationalMonitor/src/main/java/com/example/operationalmonitor/service/HelloServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.operationalmonitor.service;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.Counter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,12 +14,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class HelloServiceImpl {
-    private Counter requests = Counter.build()
-            .name("Lewis_TEST").help("Total requests.").register();
 
+    @Autowired
+    MeterRegistry meterRegistry;
 
-    public void counterIncr(){
-        requests.inc();
+    public void counterIncr() {
+        // 自定义metric
+        meterRegistry.counter("Lewis", Tags.of("tagKey","tagValue")).increment();
         System.out.println("invoke Counter#inc");
     }
 }
