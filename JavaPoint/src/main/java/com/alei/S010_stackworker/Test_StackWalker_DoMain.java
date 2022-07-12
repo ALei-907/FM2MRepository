72d808ae341f2c5c62d9690a96e3ddc851a765af
leilimin <461083868@qq.com>
2022-07-12 15:49:12 +0800

    feat(JavaPoint): StackWalker
    1.获取调用者CallerClass

--- JavaPoint/src/main/java/com/alei/S010_stackworker/Test_StackWalker_DoMain.java
+++ JavaPoint/src/main/java/com/alei/S010_stackworker/Test_StackWalker_DoMain.java
@@ -4,11 +4,13 @@ package com.alei.S010_stackworker;
  * @author LeiLiMin
  */
 public class Test_StackWalker_DoMain {
-    public static void main() throws ClassNotFoundException {
-        Class<?> main = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
-                .walk((s) -> s.filter(e -> e.getMethodName().equals("main")).findFirst().map(
-                        StackWalker.StackFrame::getDeclaringClass))
-                .orElseThrow(ClassNotFoundException::new);
+    public static void main()   {
+        // 获取到了当前堆栈信息
+        // 调用链: Test_StackWalker_DoMain  -> 栈顶 0号元素
+        //        Test_StackWalker         -> 栈底 1号元素
+        Object main = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE)
+                        .walk((s) -> s.map(StackWalker.StackFrame::getDeclaringClass).skip(1).findFirst());
+
         System.out.println(main);
     }
 }
