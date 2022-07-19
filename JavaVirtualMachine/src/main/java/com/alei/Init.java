456c62860b07df7e6fe99a7ec036835ffc12c08f
leilimin <461083868@qq.com>
2022-07-19 21:51:02 +0800

    init(JUC): 补充

--- JavaVirtualMachine/src/main/java/com/alei/Init.java
+++ JavaVirtualMachine/src/main/java/com/alei/Init.java
@@ -20,11 +20,20 @@ public class Init {
     }
 
     public static void main(String[] args) {
-        // b 8位,w 16位,l 32 位,q 64位
         /**
          * eg: 0000 00001
          * 大端字节序(0000 00001)：高字节存于内存低地址，低字节存于内存高地址
          * 小端字节序(0001 00000)：高字节存于内存高地址，低字节存于内存低地址
+         */
+
+        // b 8位,w 16位,l 32 位,q 64位
+        // ebp: 32位机器的base指针 rbp: 64位机器的base指针
+        /**
+         * sub l $1,-4(%rbp) : 语义就是将64位机器的base指针减去4byte的偏移量
+         * bp: base point(栈底)
+         * sp: stack point(栈顶)
+         * l $1: 1个32位
+         * -4: 内存的存储单位为1byte
          */
         a();
     }
