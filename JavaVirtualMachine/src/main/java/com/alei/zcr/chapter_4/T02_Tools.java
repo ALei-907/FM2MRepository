d0113e23caff9957628720008ed1e0db15de1905
leilimin <461083868@qq.com>
2022-08-19 23:11:03 +0800

    feat(JVM): 第四章
    1.jps，jstat

---
+++ JavaVirtualMachine/src/main/java/com/alei/zcr/chapter_4/T02_Tools.java
@@ -0,0 +1,15 @@
+package com.alei.zcr.chapter_4;
+
+/**
+ * @author LeiLiMin
+ * @date: 2022/8/19
+ */
+public class T02_Tools {
+    public static void main(String[] args) {
+        /**
+         * jps [options] [hostid]: 列出正在进行中的虚拟机进程
+         * jstat [options vmid interval s[ms] count]: jstat -gc 2764 250 20
+         *                                            查询2764进程的垃圾收集情况 以250msd的查询频率 查询20次
+         */
+    }
+}
