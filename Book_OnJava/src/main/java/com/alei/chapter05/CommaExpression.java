4f4e284bc0d5cabfe3e31145f45ac59e5b8baf8e
leilimin <461083868@qq.com>
2022-10-20 20:02:51 +0800

    feat: on Java
    1.label

--- Book_OnJava/src/main/java/com/alei/chapter05/CommaExpression.java
+++ Book_OnJava/src/main/java/com/alei/chapter05/CommaExpression.java
@@ -12,5 +12,20 @@ public class CommaExpression {
         for (int i = 0, j = i + 1; i < 5; i++, j++) {
             System.out.println(i + " : " + j);
         }
+
+        /**
+         * 类goto的标签
+         * 标签之后必须紧跟循环,与break与contonue配合使用
+         */
+        outter:
+        while (true) {
+            inner:
+            for (int i = 0; i < 5; i++) {
+                if (i == 2) {
+                    System.out.println("continue outter");
+                    continue outter;
+                }
+            }
+        }
     }
 }
