7a80bbb1b3bc892fcec4837ec86cf357c530719e
leilimin <461083868@qq.com>
2022-10-25 18:25:57 +0800

    feat: Class init order

---
+++ Java_QA/src/main/java/com/alei/type/T01_Byte_Yu_0XFF.java
@@ -0,0 +1,30 @@
+package com.alei.type;
+
+/**
+ * @author LeiLiMin
+ * @date: 2022/10/25
+ */
+public class T01_Byte_Yu_0XFF {
+    public static void main(String[] args) {
+        char[] chars = args[0].toCharArray();
+        byte[] changeAfter = new byte[chars.length];
+        int index = 0;
+        for (char element : chars) {
+            /**
+             * (element >> 8 & 0XFF): 通过& 0XFF使得element向上转型为一个int,就可以使得byte所表示的数转为无符号表示的byte
+             *  (byte) (element >> 8 & 0XFF): 通过byte强转,是考虑到兼容不同端序实现的物理机器
+             *      int a=1;
+             *      如果是大端字节序，则在内存中存储方式为   0000 0000    0000 0000   0000 0000    0000 0001
+             *      如果是小端字节序，则在内存中存储方式为   0000 0001    0000 0000   0000 0000    0000 0000
+             *      如果直接byte(int a),那么大端=0000 0001
+             *                            小端=0000 0000
+             *      通过0XFF转 byte        大端=小端=0000 0001
+             */
+            changeAfter[index] = (byte) (element >> 8 & 0XFF);
+            index++;
+        }
+        for (byte b : changeAfter) {
+            System.out.println(b);
+        }
+    }
+}
