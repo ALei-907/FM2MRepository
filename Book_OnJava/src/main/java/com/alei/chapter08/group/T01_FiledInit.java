f02c70f904d406fc53bb4241d5174313d19b8124
leilimin <461083868@qq.com>
2022-10-28 15:34:23 +0800

    feat(feature): java9 module

---
+++ Book_OnJava/src/main/java/com/alei/chapter08/group/T01_FiledInit.java
@@ -0,0 +1,49 @@
+package com.alei.chapter08.group;
+
+import java.util.Objects;
+
+/**
+ * @author LeiLiMin
+ * @date: 2022/10/28
+ */
+
+/**
+ * 属性的初始化方式
+ *
+ * 定义对象初始化: {@link FiledInit#a}         在构造器之前调用
+ * 构造器中初始化: {@link FiledInit#FiledInit()}
+ * 使用时初始化:   {@link FiledInit#getC()}   延迟初始化
+ * 实例初始化:     {@link FiledInit#d}
+ */
+class FiledInit {
+
+    private int a = 1;
+
+    private String b;
+
+    private String c;
+
+    private String d;
+
+
+    public FiledInit() {
+        System.out.println("Outer Create");
+        b = "Outer.b";
+    }
+
+    public String getC() {
+        if (Objects.isNull(c)) {
+            c = "Outer.c";
+        }
+        return c;
+    }
+
+    {
+        d = "Outer.d";
+    }
+}
+
+public class T01_FiledInit {
+    public static void main(String[] args) {
+    }
+}
