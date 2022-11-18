470c98d5e4c7118e7a498d23e21d9d4a73cccb4e
leilimin <461083868@qq.com>
2022-11-18 19:31:17 +0800

    feat: OnJava
    1.多态

--- Book_OnJava/src/main/java/com/alei/chapter11/T01_PointerThis.java
+++ Book_OnJava/src/main/java/com/alei/chapter11/T01_PointerThis.java
@@ -9,9 +9,15 @@ public class T01_PointerThis {
         PointerThis pointerThis = new PointerThis();
         // 使用.new语法来创建内部类
         PointerThis.InnerClass innerClass = pointerThis.new InnerClass();
-
         // 为什么如下方式不行,因为非static内部类,实例化的时候需要与外围对象建立关联,所以必须得使用外围对象来实例话内部对象
         // PointerThis.InnerClass innerClass0= new PointerThis.InnerClass();
+
+        // 对于内部私有对象,可以实现一个对外暴露的接口
+        // 这样对于内部类内部的信息不会泄露出去,但是又能通过具备访问权限的外部接口获取对应的操作
+        PrivateInnerClassSuper superInterface = pointerThis.makePrivateInnerClass();
+        superInterface.privateDo();
+        superInterface.publicDo();
+
     }
 
 
@@ -40,7 +46,29 @@ class PointerThis {
         }
     }
 
+    private class PrivateInnerClass implements PrivateInnerClassSuper {
+        @Override
+        public void publicDo() {
+            System.out.println("publicDo");
+        }
+
+        @Override
+        public void privateDo() {
+            System.out.println("privateDo");
+        }
+    }
+
     public InnerClass makeInnerClass() {
         return new InnerClass();
     }
+
+    public PrivateInnerClass makePrivateInnerClass() {
+        return new PrivateInnerClass();
+    }
+}
+
+interface PrivateInnerClassSuper {
+    void publicDo();
+
+    void privateDo();
 }
