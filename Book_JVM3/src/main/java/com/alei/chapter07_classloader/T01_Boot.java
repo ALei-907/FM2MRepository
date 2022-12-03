26f498416fb09de46dbcece25f1bf7739878ac7c
leilimin <461083868@qq.com>
2022-12-03 18:15:44 +0800

    feat: JVM

--- Book_JVM3/src/main/java/com/alei/chapter07_classloader/T01_Boot.java
+++ Book_JVM3/src/main/java/com/alei/chapter07_classloader/T01_Boot.java
@@ -22,4 +22,7 @@ public class T01_Boot {
     // HotSpot采用Oop-Klass模型来表示Java的对象和类,Oop指的是普通对象指针,Klass表示对象的具体类型,之所以这么做是不像让每个对象都包含一个虚方法表
     //   Klass: 直白点说就是通过object.getClass()所得到的数据使用一个C++实例来表示
     //          里边儿记录了父类,父接口,直接父类,子类,虚方法表等object.getClass()通用的信息
+    //   Oop:   Java对象使用Oop来表示,也就是说在Java程序运行过程中每创建一个对象,在HotspotVm内部就会创建一个oop实例来表示Java对象
+    //          Java对象内存布局主要分为header(头部)与fields(实例字段)
+    //          header由_mark和_metadata组成
 }
