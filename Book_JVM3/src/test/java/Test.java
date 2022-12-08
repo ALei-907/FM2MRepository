f7b2ca2ef8f875043ce74a2aa4c84e62cc5bc7e3
leilimin <461083868@qq.com>
2022-12-08 23:24:14 +0800

    feat: ClassForName

--- Book_JVM3/src/test/java/Test.java
+++ Book_JVM3/src/test/java/Test.java
@@ -8,24 +8,10 @@ import java.util.Stack;
  * @date: 2022/11/9
  */
 public class Test {
-    static String bracketLeftStr = "(";
-
-    public static void main(String[] args) {
-        String expression = "(1+2)+(2+1)";
-        // System.out.println(expression.charAt(0));
-        int bracketLeft = 0;
-        int index = 0;
-        while (index < expression.length()) {
-            int preValue = -1;
-            switch (expression.charAt(index)) {
-                case ' ':
-                    break;
-                case '(':
-                    bracketLeft++;
-                    break;
-            }
-            index++;
-        }
+    public static void main(String[] args) throws ClassNotFoundException {
+        // 全限定名称
+        Class<?> test = Class.forName("com.alei.t01.T01");
+        System.out.println(test.getName());
     }
 }
 
