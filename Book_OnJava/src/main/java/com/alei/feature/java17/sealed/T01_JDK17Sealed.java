1850faa65e553ff142c46af6d13a133e421eceb3
leilimin <461083868@qq.com>
2022-12-14 22:31:17 +0800

    feat: jep
    1.密封类扩展

--- Book_OnJava/src/main/java/com/alei/feature/java17/sealed/T01_JDK17Sealed.java
+++ Book_OnJava/src/main/java/com/alei/feature/java17/sealed/T01_JDK17Sealed.java
@@ -11,47 +11,55 @@ public class T01_JDK17Sealed {
     }
 }
 
-// /**
-//  * Jdk17 feature: 密封类sealed class,可以指定哪些子类可以继承该父类
-//  * sealed父类:
-//  * 1.通过sealed关键字修饰,必须通过关键字指定至少一个子类
-//  * sealed子类:
-//  * 1.必须通过以下方式来修饰子类
-//  * -: final
-//  * -: sealed
-//  * -: no-sealed
-//  * -: 枚举类
-//  */
-// sealed class SealedSuperClass permits SealedSub1, SealedSub2, SealedSealedSub1 {
-//
-// }
-//
-// /**
-//  * final: 继承SealedSuperClass的子类,是这条继承路径上的最后一个节点
-//  */
-// final class SealedSub1 extends SealedSuperClass {
-//
-// }
-//
-// final class SealedSub2 extends SealedSuperClass {
-//
-// }
-//
-// /**
-//  * sealed: 继承SealedSuperClass的子类,可以继续拓展
-//  */
-// sealed class SealedSealedSub1 extends SealedSuperClass permits SealedSealedSub1Sub {
-//
-// }
-//
-// /**
-//  * non-sealed: SealedSealedSub1Sub的子类可不受限制的继承它,也就是间接获得来顶级类SealedSuperClass的属性和行为
-//  * : 所以sealed是限制了直接子类
-//  */
-// non-sealed class SealedSealedSub1Sub extends SealedSealedSub1 {
-//
-// }
-//
-// class SealedSealedSub1SubSub1 extends SealedSealedSub1Sub {
-//
-// }
+/**
+ * Jdk17 feature: 密封类sealed class,可以指定哪些子类可以继承该父类
+ * sealed父类:
+ * 1.通过sealed关键字修饰,必须通过关键字指定至少一个子类
+ * sealed子类:
+ * 1.必须通过以下方式来修饰子类
+ * -: final
+ * -: sealed
+ * -: no-sealed
+ * -: 枚举类
+ */
+sealed class SealedSuperClass permits SealedSub1, SealedSub2, SealedSealedSub1 {
+
+}
+
+/**
+ * final: 继承SealedSuperClass的子类,是这条继承路径上的最后一个节点
+ */
+final class SealedSub1 extends SealedSuperClass {
+
+}
+
+final class SealedSub2 extends SealedSuperClass {
+
+}
+
+/**
+ * sealed: 继承SealedSuperClass的子类,可以继续拓展
+ */
+sealed class SealedSealedSub1 extends SealedSuperClass permits SealedSealedSub1Sub {
+
+}
+
+/**
+ * non-sealed: SealedSealedSub1Sub的子类可不受限制的继承它,也就是间接获得来顶级类SealedSuperClass的属性和行为
+ * : 所以sealed是限制了直接子类
+ */
+non-sealed class SealedSealedSub1Sub extends SealedSealedSub1 {
+
+}
+
+class SealedSealedSub1SubSub1 extends SealedSealedSub1Sub {
+
+}
+
+/**
+ * 枚举类实现密封接口
+ */
+sealed interface EnumInterface permits Enum{}
+enum Enum implements EnumInterface{
+
+}
